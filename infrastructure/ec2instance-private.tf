2c9c8fbab19b76f0941c36511a371e40d53fe0e6
subhasahu <88259300+subhasahu@users.noreply.github.com>
2022-01-16 16:45:36 +0530

    ec2instance-private.tf

---
+++ infrastructure/ec2instance-private.tf
@@ -0,0 +1,19 @@
+# AWS EC2 Instance Terraform Module
+# EC2 Instances that will be created in VPC Private Subnets
+module "ec2_private" {
+  depends_on = [ module.vpc ] # wait for VPC module to create and then it will create
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.4.0"
+  # insert the 10 required variables here
+  name                   = "${var.environment}-vm"
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  vpc_security_group_ids = [module.private_sg.this_security_group_id]
+  subnet_ids = [
+    module.vpc.private_subnets[0],
+    module.vpc.private_subnets[1]
+  ]  
+  instance_count         = var.private_instance_count
+  tags = local.common_tags
+}
